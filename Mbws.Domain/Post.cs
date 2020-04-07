d6cbeef243fe3abb91b03aa9ab793f7d56643e06
Mariusz Budzisz <mariusz.budzisz@yahoo.com>
2020-04-07 13:13:31 +0200

    Adding Id generation on the server side

--- Mbws.Domain/Post.cs
+++ Mbws.Domain/Post.cs
@@ -4,9 +4,9 @@ namespace Mbws.Domain
 {
     public class Post
     {
-        public Guid Id { get; set; }
+        public Guid Id { get; set; } = Guid.NewGuid();
         public string Title { get; set; }
         public string Content { get; set; }
-        public DateTime PublicationDate { get; set; }
+        public DateTime PublicationDate { get; set; } // Applied on the client-side.
     }
 }
